108e9f0f9253b5b37d33352ffc775ef48a55d51d
icedancemelody <49614958+icedancemelody@users.noreply.github.com>
2020-11-04 15:02:02 +0800

    subject_14

---
+++ src/subject_14/solution.java
@@ -0,0 +1,31 @@
+package subject_14;
+
+//找寻一个输入的字符串数组，他们的最长公共前缀
+//依次遍历每一个数组的元素，进行更新前缀，遍历到最后就是结果
+//
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+}
+
